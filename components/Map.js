9327bf520f693bbebe79b5f1ba68e78a9b62857c
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2021-12-06 03:31:13 +0600

    Added destination marker, and zoom functionality based on change in origin/destination

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
@@ -10,9 +10,20 @@ import { GOOGLE_API_KEY } from "@env";
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    // Zoom and fit to markers
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, bottom: 50, left: 50, right: 50 },
+    });
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -41,6 +52,18 @@ const Map = () => {
           title={"Origin"}
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title={"Destination"}
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
