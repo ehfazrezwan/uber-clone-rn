e8946e57dfb93ee34cc2f9647f781b68ea60a900
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2021-12-05 14:50:57 +0600

    Google Place autocomplete component built

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setOrigin, setDestination } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -17,6 +23,39 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          placeholder="Where From?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          minLength={2}
+          enablePoweredByContainer={false}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          query={{
+            key: GOOGLE_API_KEY,
+            language: "en",
+          }}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
