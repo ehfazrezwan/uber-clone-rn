ed7acc13de51c9fece6e7b100bbf73def14f62cc
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2021-12-06 03:06:42 +0600

    Built trip navigation component

---
+++ components/NavigateCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import tw from "twrnc";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/core";
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Ehfaz</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={toInputBoxStyles}
+            placeholder="Where to?"
+            debounce={400}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+
+              navigation.navigate("RideOptionsCard");
+            }}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_API_KEY,
+              language: "en",
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
+
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
