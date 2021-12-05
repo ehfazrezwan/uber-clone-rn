21a8bde5262ec97d1e5c0bf271f1a8c9842cef5a
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2021-12-06 03:22:58 +0600

    Added directions from A to B, and wrapped in KeyboardAvoidingView

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 import { useSelector } from "react-redux";
 import tw from "twrnc";
-import { selectOrigin } from "../slices/navSlice";
+import { selectDestination, selectOrigin } from "../slices/navSlice";
+import { GOOGLE_API_KEY } from "@env";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
 
   return (
     <MapView
@@ -19,6 +22,16 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_API_KEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
+
       {origin?.location && (
         <Marker
           coordinate={{
