f69679e7cf82556e34ead6f10c62276223c72632
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2021-12-06 05:20:19 +0600

    Integrated Google's distance matrix API to get travel time information

--- components/Map.js
+++ components/Map.js
@@ -2,15 +2,20 @@ import React, { useEffect, useRef } from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import tw from "twrnc";
-import { selectDestination, selectOrigin } from "../slices/navSlice";
+import {
+  selectDestination,
+  selectOrigin,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
 import { GOOGLE_API_KEY } from "@env";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
   const mapRef = useRef(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!origin || !destination) return;
@@ -20,6 +25,22 @@ const Map = () => {
       edgePadding: { top: 50, bottom: 50, left: 50, right: 50 },
     });
   }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    const getTravelTime = async () => {
+      const URL = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_API_KEY}`;
+
+      fetch(URL)
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+        });
+    };
+
+    getTravelTime();
+  }, [origin, destination, GOOGLE_API_KEY]);
 
   return (
     <MapView
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -10,7 +10,9 @@ import {
   Image,
 } from "react-native";
 import { Icon } from "react-native-elements";
+import { useSelector } from "react-redux";
 import tw from "twrnc";
+import { selectTravelTimeInformation } from "../slices/navSlice";
 
 const data = [
   {
@@ -33,9 +35,12 @@ const data = [
   },
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw`bg-white flex-1 flex-grow`}>
@@ -46,7 +51,9 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>
+          Select a Ride - {travelTimeInformation?.distance.text}{" "}
+        </Text>
       </View>
 
       <FlatList
@@ -69,14 +76,17 @@ const RideOptionsCard = () => {
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time</Text>
+              <Text>{travelTimeInformation?.duration.text}</Text>
             </View>
             <Text style={tw`text-xl`}>$99</Text>
           </TouchableOpacity>
         )}
       />
       <View>
-        <TouchableOpacity style={tw`bg-black py-3 m-3`}>
+        <TouchableOpacity
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+          disabled={!selected}
+        >
           <Text style={tw`text-center text-white text-xl`}>
             Choose {selected?.title}
           </Text>
